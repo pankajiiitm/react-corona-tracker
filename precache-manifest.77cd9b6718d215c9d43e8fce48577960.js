0bb399c1b3294ed39d7517aaf05218ff602d14f3
pankaj <vermapankaj1234567890@gmail.com>
2021-03-13 12:43:19 +0530

    Updates

---
+++ precache-manifest.77cd9b6718d215c9d43e8fce48577960.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "de19d97553a6507dc43ad83fb9ea4660",
+    "url": "/react-corona-tracker/index.html"
+  },
+  {
+    "revision": "2f0bed0d0e6e61c4675f",
+    "url": "/react-corona-tracker/static/css/2.7c35e809.chunk.css"
+  },
+  {
+    "revision": "b449c291bb3bddbd6373",
+    "url": "/react-corona-tracker/static/css/main.54d43338.chunk.css"
+  },
+  {
+    "revision": "2f0bed0d0e6e61c4675f",
+    "url": "/react-corona-tracker/static/js/2.95317930.chunk.js"
+  },
+  {
+    "revision": "68eb484dce045119a52132a8eb902e52",
+    "url": "/react-corona-tracker/static/js/2.95317930.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b449c291bb3bddbd6373",
+    "url": "/react-corona-tracker/static/js/main.8e619d58.chunk.js"
+  },
+  {
+    "revision": "8cef090ec1a33711305f",
+    "url": "/react-corona-tracker/static/js/runtime-main.4e2d5e89.js"
+  }
+]);
